520cde69ca5599ff2bba884c42e4dcd389543dc3
may <m4rch3n1ng@gmail.com>
2024-05-27 12:54:02 +0200

    error Span and Position

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,4 @@
+use std::{cmp::Ordering, fmt::Display};
 use thiserror::Error;
 
 #[derive(Debug, Error)]
@@ -65,5 +66,158 @@ impl serde::de::Error for Error {
 	{
 		let msg = msg.to_string();
 		Error::Custom(msg)
+	}
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Position {
+	pub line: usize,
+	pub col: usize,
+	pub index: usize,
+}
+
+impl Display for Position {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		write!(f, "line {}, column {}", self.line, self.col)
+	}
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Span {
+	Point(Position),
+	Span(Position, Position),
+}
+
+impl Span {
+	pub fn range(&self) -> std::ops::Range<usize> {
+		match self {
+			Span::Point(pos) => pos.index..(pos.index + 1),
+			Span::Span(pos1, pos2) => pos1.index..(pos2.index),
+		}
+	}
+}
+
+impl Display for Span {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		match self {
+			Span::Point(pos) => Display::fmt(pos, f),
+			Span::Span(pos1, pos2) => {
+				if pos1.line == pos2.line {
+					write!(
+						f,
+						"line {}, columns {} to {}",
+						pos1.line, pos1.col, pos2.col
+					)
+				} else {
+					write!(f, "{} to {}", pos1, pos2)
+				}
+			}
+		}
+	}
+}
+
+impl Ord for Span {
+	fn cmp(&self, other: &Self) -> Ordering {
+		match (self, other) {
+			(Span::Point(p1), Span::Point(p2)) => p1.cmp(p2),
+			(Span::Span(s1_1, s1_2), Span::Span(s2_1, s2_2)) => {
+				s1_1.cmp(s2_1).then_with(|| s1_2.cmp(s2_2))
+			}
+			(Span::Point(p), Span::Span(s1, s2)) => match p.cmp(s1) {
+				Ordering::Equal => Ordering::Equal,
+				Ordering::Less => Ordering::Less,
+				Ordering::Greater => {
+					if p > s2 {
+						Ordering::Greater
+					} else {
+						Ordering::Equal
+					}
+				}
+			},
+			(Span::Span(s1, s2), Span::Point(p)) => match s1.cmp(p) {
+				Ordering::Equal => Ordering::Equal,
+				Ordering::Less => {
+					if s2 < p {
+						Ordering::Less
+					} else {
+						Ordering::Equal
+					}
+				}
+				Ordering::Greater => Ordering::Greater,
+			},
+		}
+	}
+}
+
+impl PartialOrd for Span {
+	fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+		Some(self.cmp(other))
+	}
+}
+
+#[cfg(test)]
+mod test {
+	use super::{Position, Span};
+	use std::cmp::Ordering;
+
+	#[test]
+	fn ord_span() {
+		let span = Span::Span(
+			Position {
+				line: 2,
+				col: 6,
+				index: 15,
+			},
+			Position {
+				line: 2,
+				col: 16,
+				index: 25,
+			},
+		);
+
+		let p1 = Span::Point(Position {
+			line: 1,
+			col: 6,
+			index: 5,
+		});
+
+		assert_eq!(span.cmp(&p1), Ordering::Greater);
+		assert_eq!(p1.cmp(&span), Ordering::Less);
+
+		let p2 = Span::Point(Position {
+			line: 2,
+			col: 6,
+			index: 15,
+		});
+
+		assert_eq!(span.cmp(&p2), Ordering::Equal);
+		assert_eq!(p2.cmp(&span), Ordering::Equal);
+
+		let p3 = Span::Point(Position {
+			line: 2,
+			col: 11,
+			index: 20,
+		});
+
+		assert_eq!(span.cmp(&p3), Ordering::Equal);
+		assert_eq!(p3.cmp(&span), Ordering::Equal);
+
+		let p4 = Span::Point(Position {
+			line: 2,
+			col: 16,
+			index: 25,
+		});
+
+		assert_eq!(span.cmp(&p4), Ordering::Equal);
+		assert_eq!(p4.cmp(&span), Ordering::Equal);
+
+		let p5 = Span::Point(Position {
+			line: 3,
+			col: 6,
+			index: 30,
+		});
+
+		assert_eq!(span.cmp(&p5), Ordering::Less);
+		assert_eq!(p5.cmp(&span), Ordering::Greater);
 	}
 }
