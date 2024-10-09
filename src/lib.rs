1a3db40087233b4bf85a900083cff481365adf73
may <m4rch3n1ng@gmail.com>
2024-10-10 00:04:48 +0200

    document mayfig lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,44 @@
+//! # mayfig
+//!
+//! `mayfig` is a config format designed for the [`mayland`](https://github.com/m4rch3n1ng/mayland) compositor.
+//!
+//! mayfig is, at its core, a `key=value` config format and is
+//! supposed to look similar-ish to toml, hyprlang and the sway config format.
+//!
+//! ```text
+//! input {
+//!     keyboard {
+//!         # xkb_file = "~/.config/keymap/may.xkb"
+//!
+//!         repeat_delay = 600
+//!         repeat_rate = 25
+//!     }
+//!
+//!     touchpad {
+//!         tap = true
+//!
+//!         natural_scroll = true
+//!         scroll_method = "two_finger"
+//!     }
+//! }
+//!
+//! cursor {
+//!     xcursor_theme = "Bibata-Modern-Classic"
+//!     xcursor_size = 24
+//! }
+//!
+//! bind {
+//!     "mod escape" = "quit"
+//!     "mod q" = "close"
+//!
+//!     "mod t" = "spawn" [ "kitty" ]
+//!     "mod n" = "spawn" [ "firefox" ]
+//! }
+//! ```
+//!
+//! for a more thorough explanation take a look at the readme on
+//! [`github`](https://github.com/m4rch3n1ng/mayfig) or on crates.io.
+
 pub mod de;
 pub mod error;
 pub mod ser;
