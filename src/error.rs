53a23275c049b66b3a7102bdb2587029af86d34e
may <m4rch3n1ng@gmail.com>
2026-01-06 03:26:49 +0100

    don't depend on thiserror
    it's not that much more annoying, but it makes mayfig not depend on any
    proc-macro dependencies anymore.

--- src/error.rs
+++ src/error.rs
@@ -1,7 +1,6 @@
 //! when deserializing or serializing mayfig goes wrong
 
 use std::{cmp::Ordering, fmt::Display};
-use thiserror::Error;
 
 /// a `mayfig::Error`
 #[derive(Debug)]
@@ -86,100 +85,124 @@ impl From<std::io::Error> for Error {
 }
 
 /// a mayfig error code
-#[derive(Debug, Error)]
+#[derive(Debug)]
 #[non_exhaustive]
 pub enum ErrorCode {
 	/// failed to write into a [`Write`](std::io::Write).
 	///
 	/// can currently only occur when serializing with [`to_writer`](crate::to_writer).
-	#[error("io error")]
-	Io(#[source] std::io::Error),
+	Io(std::io::Error),
 
 	/// unexpected end of file while parsing
-	#[error("end of file")]
 	Eof,
 	/// mayfig value is invalid utf8
-	#[error("invalid utf8")]
 	InvalidUtf8,
 
 	/// unknown escape sequence in string
-	#[error("unknown escape sequence {0:?}")]
 	UnknownEscape(char),
 	/// unescaped control character in string
-	#[error("unescaped control character {0:?}")]
 	UnescapedControl(char),
 
 	/// expected newline, found value
-	#[error("expected newline, found {0:?} first")]
 	ExpectedNewline(char),
 	/// expected value, found newline
-	#[error("unexpected newline")]
 	UnexpectedNewline,
 
 	/// invalid boolean
-	#[error("invalid boolean {0:?}")]
 	InvalidBool(String),
 	/// invalid number
-	#[error("invalid number {0:?}")]
 	InvalidNum(String),
 	/// unexpected word
-	#[error("unexpected word {0:?}")]
 	UnexpectedWord(String),
 
 	/// expected quote
-	#[error("expected quote \" or ', got {0:?}")]
 	ExpectedQuote(char),
 	/// expected value assignment
-	#[error("expected value assignment '=' or '{{', got {0:?}")]
 	ExpectedValue(char),
 	/// expected map
-	#[error("expected map '{{', got {0:?}")]
 	ExpectedMap(char),
 	/// expected sequence
-	#[error("expected seq '[', got {0:?}")]
 	ExpectedSeq(char),
 	/// expected end of sequence
-	#[error("expected end of seq ']', got {0:?}")]
 	ExpectedSeqEnd(char),
 	/// expected enum
-	#[error("expected tagged enum, got {0:?}")]
 	ExpectedEnum(char),
 	/// expected bytes as string or sequence
-	#[error("expected quote ', \" or seq, got {0:?}")]
 	ExpectedBytes(char),
 
 	/// expected delimiter
-	#[error("expected delimiter after string, got {0:?}")]
 	ExpectedDelimiter(char),
 
 	/// expected numeric
-	#[error("expected ascii numeric, got {0:?}")]
 	ExpectedNumeric(char),
 	/// expected alphabetic
-	#[error("unquoted identifier may only start with ascii letters or _, found {0:?}")]
 	ExpectedAsciiAlphabetic(char),
 	/// expected alphanumeric
-	#[error(
-		"unquoted identifiers may only contain ascii letters, numbers, _, - or +, found {0:?}"
-	)]
 	ExpectedAsciiAlphanumeric(char),
 
 	/// unit values are unsupported in mayfig
-	#[error("unsupported unit type")]
 	UnsupportedUnit,
 	/// nan is unsupported in mayfig
-	#[error("unsupported nan")]
 	UnsupportedNaN,
 	/// `None` is unsupported in mayfig
-	#[error("unsupported none")]
 	UnsupportedNone,
 	/// unsupported map key
-	#[error("unsupported map key type {0}")]
 	UnsupportedMapKey(&'static str),
 
 	/// custom serde error
-	#[error("{0}")]
 	Custom(String),
+}
+
+impl std::error::Error for ErrorCode {
+	fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+		match self {
+			ErrorCode::Io(source) => Some(source),
+			_ => None,
+		}
+	}
+}
+
+impl Display for ErrorCode {
+	fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+		match self {
+			ErrorCode::Io(_) => f.write_str("io error"),
+			ErrorCode::Eof => f.write_str("end of file"),
+			ErrorCode::InvalidUtf8 => f.write_str("invalid utf8"),
+			ErrorCode::UnknownEscape(t) => write!(f, "unknown escape sequence {t:?}"),
+			ErrorCode::UnescapedControl(ch) => write!(f, "unescaped control character {ch:?}"),
+			ErrorCode::ExpectedNewline(t) => write!(f, "expected newline, found {t:?} first"),
+			ErrorCode::UnexpectedNewline => f.write_str("unexpected newline"),
+			ErrorCode::InvalidBool(t) => write!(f, "invalid boolean {t:?}"),
+			ErrorCode::InvalidNum(t) => write!(f, "invalid number {t:?}"),
+			ErrorCode::UnexpectedWord(t) => write!(f, "unexpected word {t:?}"),
+			ErrorCode::ExpectedQuote(t) => write!(f, "expected quote \" or ', got {t:?}"),
+			ErrorCode::ExpectedValue(t) => {
+				write!(f, "expected value assignment '=' or '{{', got {t:?}")
+			}
+			ErrorCode::ExpectedMap(t) => write!(f, "expected map '{{', got {t:?}"),
+			ErrorCode::ExpectedSeq(t) => write!(f, "expected seq '[', got {t:?}"),
+			ErrorCode::ExpectedSeqEnd(t) => write!(f, "expected end of seq ']', got {t:?}"),
+			ErrorCode::ExpectedEnum(t) => write!(f, "expected tagged enum, got {t:?}"),
+			ErrorCode::ExpectedBytes(t) => write!(f, "expected quote ', \" or seq, got {t:?}"),
+			ErrorCode::ExpectedDelimiter(t) => {
+				write!(f, "expected delimiter after string, got {t:?}")
+			}
+			ErrorCode::ExpectedNumeric(t) => write!(f, "expected ascii numeric, got {t:?}"),
+			ErrorCode::ExpectedAsciiAlphabetic(t) => {
+				f.write_str("unquoted identifier may only start with ")?;
+				write!(f, "ascii letters or _, found {t:?}")
+			}
+			ErrorCode::ExpectedAsciiAlphanumeric(t) => {
+				f.write_str("unquoted identifier may only contain ")?;
+				write!(f, "ascii letters, numbers, _, - or +, found {t:?}")
+			}
+			ErrorCode::UnsupportedUnit => f.write_str("unsupported unit type"),
+			ErrorCode::UnsupportedNaN => f.write_str("unsupported nan"),
+			ErrorCode::UnsupportedNone => f.write_str("unsupported none"),
+			ErrorCode::UnsupportedMapKey(t) => write!(f, "unsupported map key type {t}"),
+			ErrorCode::Custom(t) => write!(f, "{t}"),
+		}
+	}
 }
 
 /// an error position
